6155ed49d7589b965db94c7bdbddc19580fe0484
aaliapkin <aaliapkin@gmail.com>
2020-10-29 19:47:06 +0300

    Updates

---
+++ precache-manifest.59507f82cbf5d5d0917177a6aa1a0316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4fc534d2722efad1d48ca21e4e335d6",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "0847245a905a58aa07ff",
+    "url": "/weather-app/static/css/main.dd123e22.chunk.css"
+  },
+  {
+    "revision": "39b7aba4815a27305b72",
+    "url": "/weather-app/static/js/2.f6b7f0f7.chunk.js"
+  },
+  {
+    "revision": "355a40548b3071faf7a6b437298ae294",
+    "url": "/weather-app/static/js/2.f6b7f0f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0847245a905a58aa07ff",
+    "url": "/weather-app/static/js/main.4c9285bd.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
